d81026c71b095b4f0bdca841bda7c20fd19079ed
fedgenti@gmail.com <fedgenti@gmail.com>
2022-12-03 14:29:31 -0300

    ahora si

--- Space Invaders/Game.h
+++ Space Invaders/Game.h
@@ -4,7 +4,10 @@
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_image.h>
+
 #include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
 #include "Entity.h"
